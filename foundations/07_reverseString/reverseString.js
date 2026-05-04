5382d052064983f6d7641e5d7b2b00a6712e8c65
3ddie-papi <epiceddiekim@gmail.com>
2026-05-04 16:48:02 +0300

    Reversed the string as required

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let splittedStr = str.split("");
+  let reversedStr = splittedStr.reverse();
+  let joinedStr = reversedStr.join();
+  return joinedStr.replace(/,/g, "");
 };
 
 // Do not edit below this line
