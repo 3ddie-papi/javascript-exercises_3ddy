207a972583d8a06fbe2c5592df2f7262360f10e4
3ddie-papi <epiceddiekim@gmail.com>
2026-05-12 16:59:26 +0300

    Passed the test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let firstStr = firstInt.toString();
+    let secondStr = secondInt.toString();
+    if (firstInt < 0 || secondInt < 0  || typeof firstInt == "string" || typeof secondInt == "string" || typeof firstInt  == "object" || typeof secondInt == "object" || firstStr.includes(".") || secondStr.includes(".")) {
+     return `ERROR`;
+    }
+    let newFirstInt = 0;
+    let newSecondInt = 0;
+    if (firstInt > secondInt) {
+        newFirstInt = secondInt;
+        newSecondInt = firstInt;
+        let total = 0;
+        for (let i = newFirstInt; i <= newSecondInt; i++) {
+        total = total + i;
+        }
+        return total;
+    } 
+ 
+    let total = 0;
+    for (let i = firstInt; i <= secondInt; i++) {
+        total = total + i;
+    }
+    return total;
 };
 
 // Do not edit below this line
