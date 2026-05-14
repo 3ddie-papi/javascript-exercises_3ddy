a073f42ffbc3e4abb4fd5d4201be6553abc2f6ba
3ddie-papi <epiceddiekim@gmail.com>
2026-05-14 10:52:16 +0300

    Got the leap years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,17 +1,14 @@
 const leapYears = function(year) {
-  for (let i = year; i < 2025; i++) {
-    if (i % 4 == 0 && (i % 100 == 0 && i % 400 == 0)) {
-    console.log (`${year} is leap year!`);
-  } else if (i % 4 == 0 && i % 100 != 0 ) {
-    console.log(`${year} is leap year!`) ;
+  
+  if (year % 4 == 0 && (year % 100 == 0 && year % 400 == 0)) {
+    return true;
+  } else if (year % 4 == 0 && year % 100 != 0 ) {
+    return true;
   } else {
-    // return false;
+    return false;
   }
-  }
-  
-  
+    
 };
-leapYears(2001);
 
 
 // Do not edit below this line
