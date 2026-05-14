c93938257f5044610a9c1b0e7d8c0be8a72f4367
3ddie-papi <epiceddiekim@gmail.com>
2026-05-14 10:40:51 +0300

    Two functions done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsius = (num - 32) * (5/9);
+  let roundedCelsius = celsius.toFixed(1);
+  let answer = Number(roundedCelsius);
+  return answer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+   let fahrenheit = (num * (9/5)) + 32;
+   let roundedFahrenheit = fahrenheit.toFixed(1);
+   let answer = Number(roundedFahrenheit);
+   return answer
 };
 
+console.log(convertToCelsius(-100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
